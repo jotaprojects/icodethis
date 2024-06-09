d890df80a72c8bce0b06d208a1e6e7a8bedc766b
jotaprojects <jotaprojects@gmail.com>
2024-06-09 20:19:43 +0200

    Add basics

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         "login-card": resolve(__dirname, "login-card/index.html"),
         "birthday-list": resolve(__dirname, "birthday-list/index.html"),
         toasts: resolve(__dirname, "toasts/index.html"),
+        basics: resolve(__dirname, "basics/index.html"),
         "website-comps": resolve(__dirname, "website-comps/index.html"),
       },
     },
