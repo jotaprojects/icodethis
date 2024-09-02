fce656c6d0676934fb9e3c93effa90b615611c3d
jotaprojects <jotaprojects@gmail.com>
2024-09-02 23:50:45 +0200

    Add Purrson to tabbed-ui

--- tabbed-ui/script.js
+++ tabbed-ui/script.js
@@ -117,3 +117,88 @@ colors.forEach((color) => {
 async function copyToClipboard(text) {
   await navigator.clipboard.writeText(text);
 }
+
+class PurrsonScore {
+  constructor(storeKey) {
+    this.storeKey = storeKey;
+    this.createTooltip();
+    this.setScoreAsArray();
+    this.initBtns();
+  }
+
+  storeKey = "";
+  scoreArr = [];
+  tooltip = null;
+
+  initBtns() {
+    const btns = document.querySelectorAll(".btn--purrson");
+    btns.forEach((btn) => {
+      btn.appendChild(this.tooltip);
+      btn.addEventListener("click", () => {
+        btn.classList.add("found");
+        this.setScore();
+        setTimeout(() => {
+          btn.classList.remove("found");
+        }, 1000);
+      });
+    });
+  }
+
+  reset() {
+    this.scoreArr = [];
+    this.tooltip.innerText = "You found me!";
+  }
+
+  getScoreAsArray() {
+    return this.scoreArr;
+  }
+
+  setScoreAsArray() {
+    this.reset();
+
+    const storedScore = this.getLocalStorage();
+    if (storedScore) {
+      this.scoreArr = storedScore.split(",");
+    }
+  }
+
+  getScore() {
+    return this.scoreArr.length;
+  }
+
+  setScore() {
+    this.setScoreAsArray();
+    console.log("setScore", this.scoreArr, this.hasFound());
+    if (this.hasFound()) {
+      this.tooltip.innerText = "Already found!";
+      return;
+    }
+    this.scoreArr.push(this.storeKey);
+    localStorage.setItem("purrson-score", this.scoreArr);
+  }
+
+  hasFound() {
+    return this.scoreArr.includes(this.storeKey);
+  }
+
+  createTooltip() {
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("purrson-tooltip");
+    this.tooltip = tooltip;
+  }
+
+  getTooltip() {
+    return this.tooltip;
+  }
+
+  getLocalStorage() {
+    return localStorage.getItem("purrson-score");
+  }
+
+  setLocalStorage(score) {
+    localStorage.setItem("purrson-score", score);
+  }
+}
+
+const scoreKey = "purrson-tabbed-ui";
+const purrson = new PurrsonScore(scoreKey);
